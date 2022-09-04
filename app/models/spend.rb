09a6f161e79f7792ecdc60343da9ccbc30a4b4d7
Tadesse-Alemayehu <email24t@gmail.com>
2022-09-04 23:20:35 +0300

    Add: associaion and validation for spends model

--- app/models/spend.rb
+++ app/models/spend.rb
@@ -1,3 +1,7 @@
 class Spend < ApplicationRecord
-  belongs_to :author
+  belongs_to :author, class_name: :user, foreign_key: :author
+  has_many :categories, through: :category_spends
+
+  validates :name, presence: true, null: false
+  validates :amount, presence: true, comparison: { greater_than: 0 }
 end
--- db/migrate/20220904201326_create_spends.rb
+++ db/migrate/20220904201326_create_spends.rb
@@ -1,11 +1,12 @@
 class CreateSpends < ActiveRecord::Migration[7.0]
   def change
     create_table :spends do |t|
-      t.references :author, null: false, foreign_key: true
+      t.references :author, references: :users, null: false, foreign_key: true
       t.string :name
       t.integer :amount
 
       t.timestamps
     end
+    add_foreign_key :spends, :users, column: :author_id
   end
 end
