62bf428d8e030db4e3c01d018f8ea95c56f80ff9
Tadesse-Alemayehu <email24t@gmail.com>
2022-09-04 23:44:46 +0300

    Complete: migration and models

--- db/migrate/20220904202221_create_category_spends.rb
+++ db/migrate/20220904202221_create_category_spends.rb
@@ -1,12 +1,10 @@
 class CreateCategorySpends < ActiveRecord::Migration[7.0]
   def change
     create_table :category_spends do |t|
-      t.references :categorys, null: false, foreign_key: true
+      t.references :categories, null: false, foreign_key: true
       t.references :spends, null: false, foreign_key: true
 
       t.timestamps
     end
-    add_foreign_key :category_spends, :categories
-    add_foreign_key :category_spends, :spends
   end
 end
