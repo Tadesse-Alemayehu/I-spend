ed39f8a88dbcf4a817f087a671d6bbf8b347c17c
Tadesse-Alemayehu <email24t@gmail.com>
2022-09-05 23:47:49 +0300

    Update: seed with icon and more data

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,14 +9,14 @@
 admin=User.create!(name: "tadesse alemayehu", email: 'admin@gmail.com', password: '123456')
 user_one=User.create!(name: "tadesse alemayehu", email: 'user@gmail.com', password: '123456')
 
-category_one=Category.create!(user: admin, name: 'MC donald')
-category_two=Category.create!(user: admin, name: 'Tomocka')
-category_three=Category.create!(user: admin, name: 'Bety shiro')
+admin_category_one=Category.create!(user: admin, name: 'MC donald', icon: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQqx04Z9VivrrDcYmP3D3TygffvlxLwPKKzAw&usqp=CAU')
+admin_category_three=Category.create!(user: admin, name: 'Bety shiro', icon: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQqx04Z9VivrrDcYmP3D3TygffvlxLwPKKzAw&usqp=CAU')
+admin_category_two=Category.create!(user: admin, name: 'Tomocka', icon: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQqx04Z9VivrrDcYmP3D3TygffvlxLwPKKzAw&usqp=CAU')
 
 
-category_one=Category.create!(user: user_one, name: 'MC donald')
-category_two=Category.create!(user: user_one, name: 'Tomocka')
-category_three=Category.create!(user: user_one, name: 'Bety shiro')
+category_one=Category.create!(user: user_one, name: 'MC donald', icon: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQqx04Z9VivrrDcYmP3D3TygffvlxLwPKKzAw&usqp=CAU')
+category_two=Category.create!(user: user_one, name: 'Tomocka', icon: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQqx04Z9VivrrDcYmP3D3TygffvlxLwPKKzAw&usqp=CAU')
+category_three=Category.create!(user: user_one, name: 'Bety shiro', icon: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQqx04Z9VivrrDcYmP3D3TygffvlxLwPKKzAw&usqp=CAU')
 
 spend_one=Spend.create!(name: 'spend by user one 1', user: user_one, amount: 100)
 CategorySpend.create!(spend: spend_one, category: category_one)
@@ -24,6 +24,8 @@ spend_two=Spend.create!(name: 'spend by user one 2', user: user_one, amount: 270
 CategorySpend.create!(spend: spend_two, category: category_one)
 CategorySpend.create!(spend: spend_two, category: category_two)
 spend_three=Spend.create!(name: 'spend by admin 1', user: admin, amount: 320)
-CategorySpend.create!(spend: spend_three, category: category_one)
-CategorySpend.create!(spend: spend_three, category: category_two)
-CategorySpend.create!(spend: spend_three, category: category_three)
+CategorySpend.create!(spend: spend_three, category: admin_category_one)
+CategorySpend.create!(spend: spend_three, category: admin_category_two)
+CategorySpend.create!(spend: spend_three, category: admin_category_three)
+spend_three=Spend.create!(name: 'spend by admin 2', user: admin, amount: 150)
+CategorySpend.create!(spend: spend_three, category: admin_category_one)
