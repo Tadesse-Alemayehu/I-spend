04e7aeb6960fb9a617779ae7286f6c0fbb5e58d6
Tadesse-Alemayehu <email24t@gmail.com>
2022-09-06 22:22:08 +0300

    Update: login routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,10 @@ class UsersController < ApplicationController
   redirect_to user_categories_path(user_id: current_user) if user_signed_in?
   end
   def show
-  render :index
+   if user_signed_in?
+    redirect_to user_categories_path(user_id: current_user)
+   else
+    render :index
+   end
   end
 end
