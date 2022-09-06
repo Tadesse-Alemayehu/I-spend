e86620b20c9559c11685091688d6a2adcccf7c34
Tadesse-Alemayehu <email24t@gmail.com>
2022-09-06 20:55:04 +0300

    Add: Controller test

--- spec/requests/spends_spec.rb
+++ spec/requests/spends_spec.rb
@@ -1,31 +1,13 @@
 require 'rails_helper'
 
+# signed in user are going to be validated with system test. this controller tests
+# only test if a route is working fine
+
 RSpec.describe "Spends", type: :request do
   describe "GET /index" do
-    it "returns http success" do
-      get "/spends/index"
-      expect(response).to have_http_status(:success)
-    end
-  end
-
-  describe "GET /new" do
-    it "returns http success" do
-      get "/spends/new"
-      expect(response).to have_http_status(:success)
-    end
-  end
-
-  describe "GET /create" do
-    it "returns http success" do
-      get "/spends/create"
-      expect(response).to have_http_status(:success)
-    end
-  end
-
-  describe "GET /destroy" do
-    it "returns http success" do
-      get "/spends/destroy"
-      expect(response).to have_http_status(:success)
+    it "redirect to login page" do
+      get user_categories_path(:user_id => 1)
+      expect(response).to have_http_status(:redirect)
     end
   end
 
--- spec/requests/user_spec.rb
+++ spec/requests/user_spec.rb
@@ -1,25 +1,13 @@
 require 'rails_helper'
 
+# signed in user are going to be validated with system test. this controller tests
+# only test if a route is working fine
+
 RSpec.describe "Users", type: :request do
   describe "GET /index" do
     it "returns http success" do
-      get "/user/index"
+      get users_path
       expect(response).to have_http_status(:success)
     end
   end
-
-  describe "GET /show" do
-    it "returns http success" do
-      get "/user/show"
-      expect(response).to have_http_status(:success)
-    end
-  end
-
-  describe "GET /new" do
-    it "returns http success" do
-      get "/user/new"
-      expect(response).to have_http_status(:success)
-    end
-  end
-
 end
