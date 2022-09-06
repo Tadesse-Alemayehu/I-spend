68634fe9c1c20abad3d2a650bd100f5c72fef07c
Tadesse-Alemayehu <email24t@gmail.com>
2022-09-06 20:23:50 +0300

    Test: spend/transaction model

--- spec/models/spend_spec.rb
+++ spec/models/spend_spec.rb
@@ -1,13 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe Spend, type: :model do
-  describe "test description" do
+  describe "Test spend/transaction modal for save and retrive" do
       before :all do
-        user=User.first
-        @spend=Spend.new(name: 'spend by user one', user: user, amount: 200)
+        @user=User.first
       end
       it "expect spend to be saved" do
-        expect(@spend.save).to be_truthy
+        spend=Spend.new(name: 'spend by user one', user: @user, amount: 200)
+        expect(spend.save).to be_truthy
+      end
+      it "expect spend to be found" do
+        spend=Spend.create(name: 'spend by user one', user: @user, amount: 200)
+        expect(Spend.find(spend.id)).to eql(spend)
+      end
+      it "expect spend to bfail saving withour name" do
+        spend=Spend.create(user: @user, amount: 200)
+        expect(spend.save).to be_falsey
       end
     end
 end
