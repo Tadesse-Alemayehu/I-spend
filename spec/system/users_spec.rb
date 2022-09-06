702eca3bd456563018e7946de9cc74843825e02c
Tadesse-Alemayehu <email24t@gmail.com>
2022-09-07 01:31:34 +0300

    Add: system test for user

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -1,9 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe "Users", type: :system do
-  before do
-    driven_by(:rack_test)
-  end
+     describe "Test sign up" do
+      before(:all){visit new_user_registration_path}
+      it "Assert user access the page with appropriate text" do
+        expect(page).to have_content("Sign up")
+      end
+      # it "Assert user access login link on sign up page" do
+      #   expect(page).to have_xpath("//a[@href='/users/sign_in']")
+      # end
+      # it "Assert user access sign up button" do
+      #   expect(page).to have_xpath("//input[@name='commit' and @value='Sign up']")
+      #   puts page
+      # end
+      # it "Assert user is able to register with Name, Email, Password" do
+      # end
+    end
 
-  pending "add some scenarios (or delete) #{__FILE__}"
 end
