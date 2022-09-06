edd5910f1411da75fe9159eb5f44c267ac020344
Tadesse-Alemayehu <tadalem26@gmail.com>
2022-09-06 16:18:49 +0300

    Add: mdal test for user

--- spec/models/users_spec.rb
+++ spec/models/users_spec.rb
@@ -1,12 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  describe "test description" do
+  describe "Test user model for creating upadting and deleting user" do
       before :all do
         @user=User.new(name: "tadesse alemayehu", email: 'testuser@testuser.com', password: '123456')
       end
-      it "expect spend to be saved" do
+      it "expect user to be saved" do
         expect(@user.save).to be_truthy
+      end
+      it "expect user to be found by email" do
+        puts @user.save!
+        puts "user with email is"
+        puts User.where(email: 'testuser@testuser.com')
+        expect(User.find_by(email: 'testuser@testuser.com')).to eql(@user)
       end
     end
 end
