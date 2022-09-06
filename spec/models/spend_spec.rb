da1dcc61d7fb9507ce910803c90bab163d6183d8
Tadesse-Alemayehu <email24t@gmail.com>
2022-09-06 20:31:20 +0300

    Test: category model

--- spec/models/spend_spec.rb
+++ spec/models/spend_spec.rb
@@ -13,7 +13,7 @@ RSpec.describe Spend, type: :model do
         spend=Spend.create(name: 'spend by user one', user: @user, amount: 200)
         expect(Spend.find(spend.id)).to eql(spend)
       end
-      it "expect spend to bfail saving withour name" do
+      it "expect spend to fail saving without name" do
         spend=Spend.create(user: @user, amount: 200)
         expect(spend.save).to be_falsey
       end
