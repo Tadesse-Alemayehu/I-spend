4d9f88e54fcbf9cec2ddcef74142469fae4dd2e7
Tadesse-Alemayehu <email24t@gmail.com>
2022-09-08 19:59:41 +0300

    Style: final touch and seed update with icon

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,14 +9,14 @@
 admin=User.create!(name: "tadesse alemayehu", email: 'admin@gmail.com', password: '123456')
 user_one=User.create!(name: "tadesse alemayehu", email: 'user@gmail.com', password: '123456')
 
-admin_category_one=Category.create!(user: admin, name: 'MC donald', icon: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQqx04Z9VivrrDcYmP3D3TygffvlxLwPKKzAw&usqp=CAU')
-admin_category_three=Category.create!(user: admin, name: 'Bety shiro', icon: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQqx04Z9VivrrDcYmP3D3TygffvlxLwPKKzAw&usqp=CAU')
-admin_category_two=Category.create!(user: admin, name: 'Tomocka', icon: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQqx04Z9VivrrDcYmP3D3TygffvlxLwPKKzAw&usqp=CAU')
+admin_category_one=Category.create!(user: admin, name: 'MC donald', icon: 'https://logolook.net/wp-content/uploads/2021/06/McDonalds-Logo.png')
+admin_category_three=Category.create!(user: admin, name: 'Bety shiro', icon: 'https://static.wixstatic.com/media/fa00aa_ff265a3c4b1f4baf880ea8a5f92d66ca~mv2.jpg/v1/fit/w_1000%2Ch_1000%2Cal_c%2Cq_80/file.jpg')
+admin_category_two=Category.create!(user: admin, name: 'Tomocka', icon: 'https://m.media-amazon.com/images/I/71Zw8US5oIL._SL1200_.jpg')
 
 
-category_one=Category.create!(user: user_one, name: 'MC donald', icon: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQqx04Z9VivrrDcYmP3D3TygffvlxLwPKKzAw&usqp=CAU')
-category_two=Category.create!(user: user_one, name: 'Tomocka', icon: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQqx04Z9VivrrDcYmP3D3TygffvlxLwPKKzAw&usqp=CAU')
-category_three=Category.create!(user: user_one, name: 'Bety shiro', icon: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQqx04Z9VivrrDcYmP3D3TygffvlxLwPKKzAw&usqp=CAU')
+category_one=Category.create!(user: user_one, name: 'MC donald', icon: 'https://logolook.net/wp-content/uploads/2021/06/McDonalds-Logo.png')
+category_two=Category.create!(user: user_one, name: 'Tomocka', icon: 'https://static.wixstatic.com/media/fa00aa_ff265a3c4b1f4baf880ea8a5f92d66ca~mv2.jpg/v1/fit/w_1000%2Ch_1000%2Cal_c%2Cq_80/file.jpg')
+category_three=Category.create!(user: user_one, name: 'Bety shiro', icon: 'https://m.media-amazon.com/images/I/71Zw8US5oIL._SL1200_.jpg')
 
 spend_one=Spend.create!(name: 'spend by user one 1', user: user_one, amount: 100)
 CategorySpend.create!(spend: spend_one, category: category_one)
