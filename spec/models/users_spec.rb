4c6fe51d36eddd59efe631f0f354c9048d296d64
Tadesse-Alemayehu <email24t@gmail.com>
2022-09-08 20:31:34 +0300

    Assert: all test are passing

--- spec/models/users_spec.rb
+++ spec/models/users_spec.rb
@@ -13,7 +13,10 @@ RSpec.describe User, type: :model do
       expect(User.find(new_user.id)).to eql(new_user)
     end
     it 'expect user fail to save with repeated email' do
-      new_user2 = User.create(name: 'tadesse alemayehu', email: 'testuser@testuser.com', password: '123456')
+      new_user = User.new(name: 'tadesse alemayehu', email: 'testuser@testuser.com', password: '123456')
+      new_user2 = User.new(name: 'tadesse alemayehu', email: 'testuser@testuser.com', password: '123456')
+      new_user.save
+      new_user2.save
       expect(new_user2.save).to be_falsey
     end
     it 'expect user fail to save without email' do
